f1f472e6141452230655f4d17c91d532884a0c36
MuhammadUmar@MacbookPro <umar.sheikh.muhammad@gmail.com>
2020-06-28 01:15:24 +0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Muhammad Umar on 6/28/20.
+//  Copyright © 2020 Muhammad Umar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
