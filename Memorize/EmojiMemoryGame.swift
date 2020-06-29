0a5d44ee2f9fdf82aaa2c0568defe27f0394e467
MuhammadUmar@MacbookPro <umar.sheikh.muhammad@gmail.com>
2020-06-30 00:41:37 +0500

    added view model

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Muhammad Umar on 6/28/20.
+//  Copyright © 2020 Muhammad Umar. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃", "🕷", "🦇", "🐈"]
+        let numberOfPairs = Int.random(in: 2...5)
+        return MemoryGame<String>(numberOfPairOfCards: numberOfPairs) {pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
